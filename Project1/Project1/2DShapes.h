8f5a6057858777c16a2e9f33de428003f9d94e30
Xydrel <jasonh78@gmail.com>
2019-07-23 08:24:54 -0700

    MOD: reoganization of class methods and descriptions added above method types

--- Project1/Project1/2DShapes.h
+++ Project1/Project1/2DShapes.h
@@ -6,17 +6,28 @@ namespace basicshapes
 class Rectangle
 {
 public:
-	Rectangle() {}
+	// ctors
+	Rectangle() { _length = 1; _bredth = 1; }
 	Rectangle(float len, float br) : _length(len), _bredth(br) {}
 
-	const float GetRectArea() { return _length * _bredth; }
+	// dtors
+	~Rectangle() {}
+	
+	// accessors
 	const float* GetRectLength() { return &_length; }
 	const float* GetRectBredth() { return &_bredth; }
 
+	// facilitators
+	const float GetRectArea() { return _length * _bredth; }
+	const float GetPerimeter() { return 2 * (_length + _bredth); }
+
+	// mutators
 	void SetRectLength(float len) { _length = len; }
 	void SetRectBredth(float br) { _bredth = br; }
+	void ChangeRectLength(float newLen) { _length = newLen; }
 
 private:
+	// members
 	float _length;
 	float _bredth;
 };
